b691a06da470cd316f32528ea96bde0ffae7601b
gonza.otc <gonza.otc@gmail.com>
2021-10-31 17:52:52 -0300

    advanced

--- scripts/base.js
+++ scripts/base.js
@@ -1,10 +1,9 @@
 // CALCULADORA DE RENTABILIDAD DE TARJETAS GRAFICAS PARA MINADO DE CRYPTOMONEDAS EN ARGENTINA
 
-// PROYECT TODO: 
-// Aún no tengo claros todos los impuestos que deben sumarse para la compra del producto en el exterior y su posterior ingreso al país, prontamente serán tenidos en cuenta correctamente. 
-// La pagina informará el precio comprando el producto en el exterior y también comprandolo en el país. 
+// PROYECT TODO:
+// Aún no tengo claros todos los impuestos que deben sumarse para la compra del producto en el exterior y su posterior ingreso al país, prontamente serán tenidos en cuenta correctamente.
+// La pagina informará el precio comprando el producto en el exterior y también comprandolo en el país.
 // Más adelante, (se intentara) incorporar scraping para obtener los precios actuales en tiempo real. (del hardware, de las cryptomonedas y del dolar en pesos Argentinos.)
-
 
 //CONSTANTES
 const DOLARBLUE = 195;
@@ -12,39 +11,33 @@ const DOLAROFICIAL = 110;
 const KWH = 0.1;
 const HORAS = 24; //Cantidad de horas que se usará por día.
 const DIAS = 30; //Cantidad de días que se usará por mes.
-const IVA = 1.21; // Multiplicador equivalente a 21%. 
-const IMPUESTOPAIS = 1.30; //Multiplicador equivalente a 30%
+const IVA = 1.21; // Multiplicador equivalente a 21%.
+const IMPUESTOPAIS = 1.3; //Multiplicador equivalente a 30%
 
 // Operaciones básicas sobre las tarjetas.
-for (let tarjeta of arrayTarjetas){
-    tarjeta.consumoMensual = ((tarjeta.consumoWatts * HORAS * DIAS) / 1000) * DOLARBLUE * KWH;
-    tarjeta.produccionMensual = tarjeta.hashrate * DIAS * DOLARBLUE;
-    tarjeta.gananciaMensual = tarjeta.produccionMensual - tarjeta.consumoMensual;
-    tarjeta.rentabilidad = Math.floor(tarjeta.precio / tarjeta.gananciaMensual);
+for (let tarjeta of arrayTarjetas) {
+  tarjeta.consumoMensual =
+    ((tarjeta.consumoWatts * HORAS * DIAS) / 1000) * DOLARBLUE * KWH;
+  tarjeta.produccionMensual = tarjeta.hashrate * DIAS * DOLARBLUE;
+  tarjeta.gananciaMensual = tarjeta.produccionMensual - tarjeta.consumoMensual;
+  tarjeta.rentabilidad = Math.floor(tarjeta.precio / tarjeta.gananciaMensual);
 }
 
-function buscarTarjeta(){
+function buscarTarjeta() {}
 
-}
-
-
-function ordenarTarjetas(){
-
-}
-
-
+function ordenarTarjetas() {}
 
 //MAIN DEL PROGRAMA.
-//Genero las cards. 
+//Genero las cards.
 
-for (let tarjeta of arrayTarjetas){
-    const container = document.querySelector('#cards-container');
+for (let tarjeta of arrayTarjetas) {
+  const container = document.querySelector("#cards-container");
 
-    const card = document.createElement('div');
-    card.classList.add('card');
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-    //CARD DEL PRODUCTO
-    card.innerHTML = `
+  //CARD DEL PRODUCTO
+  card.innerHTML = `
     <div class="image-container">
         <img class="card-image" src="${tarjeta.img}">
         <p class="nombre">${tarjeta.nombre}</p>
@@ -83,18 +76,17 @@ for (let tarjeta of arrayTarjetas){
         </div>
             
     </div>
-    `
-    container.appendChild(card);
+    `;
+  container.appendChild(card);
 
-    //EVENTOS DEL PRODUCTO
-    
-    //Selecciono el botón de añadir al carrito de la tarjeta
-    const botonAñadir = document.getElementById(`${tarjeta.id}`);
-    //Si se clickea el botón, se añade la tarjeta al carrito.
-    botonAñadir.addEventListener(`click`, () => agregarAlCarrito(`${tarjeta.id}`));
+  //EVENTOS DEL PRODUCTO
+
+  //Selecciono el botón de añadir al carrito de la tarjeta
+  const botonAñadir = document.getElementById(`${tarjeta.id}`);
+  //Si se clickea el botón, se añade la tarjeta al carrito.
+  botonAñadir.addEventListener(`click`, () =>
+    agregarAlCarrito(`${tarjeta.id}`)
+  );
 }
 
-//CARRITO en archivo separado. 
-
-
-
+//CARRITO en archivo separado.
