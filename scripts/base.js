01e1618ab472241a7741dfd2a7a3b6b4c123d92f
gonzaotc <gonza.otc@gmail.com>
2021-11-01 23:21:56 -0300

    lot of changes.

--- scripts/base.js
+++ scripts/base.js
@@ -1,92 +1,230 @@
-// CALCULADORA DE RENTABILIDAD DE TARJETAS GRAFICAS PARA MINADO DE CRYPTOMONEDAS EN ARGENTINA
+// BUTTONS
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+// TO CHANGE DISPLAY NUMBER/TEXT
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+// TO SHOW / HIDE
+const cartOverlay = document.querySelector(".cart-overlay");
+// Le pongo "DOM" en el nombre para no confundir con el carrito
+// arreglo en el que guardo los productos.
+const cartDOM = document.querySelector(".cart");
+// TO APPEND MY DINAMIC ITEMS
+const container = document.querySelector(".products");
+const cartContent = document.querySelector(".cart-content");
 
-// PROYECT TODO:
-// Aún no tengo claros todos los impuestos que deben sumarse para la compra del producto en el exterior y su posterior ingreso al país, prontamente serán tenidos en cuenta correctamente.
-// La pagina informará el precio comprando el producto en el exterior y también comprandolo en el país.
-// Más adelante, (se intentara) incorporar scraping para obtener los precios actuales en tiempo real. (del hardware, de las cryptomonedas y del dolar en pesos Argentinos.)
+// cart
+let cart = [];
+// buttons
+let buttonsDOM = [];
 
 //CONSTANTES
 const DOLARBLUE = 195;
 const DOLAROFICIAL = 110;
 const KWH = 0.1;
-const HORAS = 24; //Cantidad de horas que se usará por día.
-const DIAS = 30; //Cantidad de días que se usará por mes.
+const HOURS = 24; //Cantidad de horas que se usará por día.
+const DAYS = 30; //Cantidad de días que se usará por mes.
 const IVA = 1.21; // Multiplicador equivalente a 21%.
 const IMPUESTOPAIS = 1.3; //Multiplicador equivalente a 30%
 
-// Operaciones básicas sobre las tarjetas.
-for (let tarjeta of arrayTarjetas) {
-  tarjeta.consumoMensual =
-    ((tarjeta.consumoWatts * HORAS * DIAS) / 1000) * DOLARBLUE * KWH;
-  tarjeta.produccionMensual = tarjeta.hashrate * DIAS * DOLARBLUE;
-  tarjeta.gananciaMensual = tarjeta.produccionMensual - tarjeta.consumoMensual;
-  tarjeta.rentabilidad = Math.floor(tarjeta.precio / tarjeta.gananciaMensual);
+// Operaciones básicas sobre los productos.
+for (let product of products) {
+  this.consumption =
+    ((this.consumption * HOURS * DAYS) / 1000) * DOLARBLUE * KWH;
+  this.production = this.hashrate * DAYS * DOLARBLUE;
+  this.income = this.production - this.consumption;
+  this.rentability = Math.floor(this.price / this.income);
 }
 
-function buscarTarjeta() {}
+// La clase User Interface guarda todos mis metodos.
+class UI {
+    //Method to display the products
+    displayProducts(products) {
+        for (let product of products) {
+            const card = document.createElement("article");
+            // la clase card contiene los estilos de la card.
+            card.classList.add("card");
+            // añado la clase marca para filtrar luego.
+            card.classList.add(product.brand);
 
-function ordenarTarjetas() {}
-
-//MAIN DEL PROGRAMA.
-//Genero las cards.
-
-for (let tarjeta of arrayTarjetas) {
-  const container = document.querySelector("#cards-container");
-
-  const card = document.createElement("div");
-  card.classList.add("card");
-
-  //CARD DEL PRODUCTO
-  card.innerHTML = `
-    <div class="image-container">
-        <img class="card-image" src="${tarjeta.img}">
-        <p class="nombre">${tarjeta.nombre}</p>
+            //card de los productos
+            card.innerHTML = `
+    <div class="card-img-container">
+        <img class="card-image" src="${product.img}">
+        <p class="card-name">${product.name}</p>
     </div>
-    <div class="info-container">
+    <div class="card-info-container">
         <div class="card-header">
-            <div class="marca ${tarjeta.marca}">
-                <img class = "marca-icon" src="icons/${tarjeta.marca}_icon.png">
-                </p class = "marca-nombre" >${tarjeta.marca}</p>
+            <div class="brand ${product.brand}">
+                <img class ="brand-icon" src="icons/${product.brand}_icon.png">
+                </p class ="brand-name" >${product.brand}</p>
             </div>
-            <div class="button-container">
-                <button id ="gaming-icon" class="material-icons md-30">
+            <div class="card-buttons-container">
+                <button class="gaming-icon material-icons-round" data-id=${product.id}>
                     sports_esports
                 </button>
-                <button id ="mining-icon" class="material-icons md-36">
+                <button class="mining-icon material-icons-round" data-id=${product.id}>
                     <img class="icon-image" src="icons/pickaxe_icon.png">
                 </button>
             </div>
         </div>
-        <div class="info-gaming">
-            <p>FPS promedio: ${tarjeta.gaming}</p>
-            <p>extra info gaming</p>
+        <div class="card-gaming-container">
         </div>
-        <div class="info-minado">
-            <p>hashrate: ${tarjeta.hashrate} usd/día</p>
-            <p>consumo: ${tarjeta.consumoWatts} watts</p>
-            <p>ganancia mensual: ${tarjeta.gananciaMensual}$</p>
-            <p>meses rentabilidad: ${tarjeta.rentabilidad}</p>
+        <div class="card-mining-container">
+            <p>hashrate: ${product.hashrate} usd/day</p>
+            <p>consumption: ${product.consumption} watts</p>
+            <p>income: ${product.income}$</p>
+            <p>rentability: ${product.rentability}</p>
         </div>
-        <p>precio: ${tarjeta.precio}$</p>
-        <div class="compra-container">
-            <button id="${tarjeta.id}" class="material-icons md-36">
+        <p>price: ${product.price}$</p>
+        <div class="card-buy-container">
+            <button class="buy-btn material-icons-round" data-id=${product.id}>
                 shopping_cart
+                <p class="card-buy-text">add to cart<p>
             </button>
-            <p>añadir al carrito<p>
-        </div>
             
+        </div>
     </div>
     `;
-  container.appendChild(card);
+            container.appendChild(card);
+        }
+    }
+    //Method to make buy buttons work with the shopping cart.
+    getBuyButtons() {
+        const buttons = [...document.querySelectorAll(".buy-btn")];
+        buttonsDOM = buttons;
+        buttons.forEach((button) => {
+            let id = button.dataset.id;
+            let inCart = cart.find((item) => item.id === id);
+            if (inCart) {
+                button.innerText = "In Cart";
+                button.disabled = true;
+            }
+            button.addEventListener("click", (event) => {
+                event.target.innerText = "In Cart";
+                event.target.disabled = true;
 
-  //EVENTOS DEL PRODUCTO
+                //get product from products
+                let cartItem = { ...Storage.getProduct(id), amount: 1 };
 
-  //Selecciono el botón de añadir al carrito de la tarjeta
-  const botonAñadir = document.getElementById(`${tarjeta.id}`);
-  //Si se clickea el botón, se añade la tarjeta al carrito.
-  botonAñadir.addEventListener(`click`, () =>
-    agregarAlCarrito(`${tarjeta.id}`)
-  );
+                //add product to the cart
+                cart = [...cart, cartItem];
+                console.log(cart);
+
+                //save cart in local storage
+                Storage.saveCart(cart);
+
+                // set cart values
+                this.setCartValues(cart);
+
+                // display cart item
+                this.addCartItem(cartItem);
+
+                // show the cart (change css propertys)
+                this.showCart();
+            });
+        });
+    }
+    //Update the cart values 
+    setCartValues(cart) {
+        let tempTotal = 0;
+        let itemsTotal = 0;
+        cart.map((item) => {
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount;
+        });
+
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
+    }
+    addCartItem(item) {
+        const div = document.createElement("div");
+        div.classList.add("cart-item");
+        div.innerHTML = `
+        <img src="${item.image}" alt="product image">
+        <div>
+            <h4>${item.title}</h4>
+            <h5>${item.price}$</h5>
+            <span class="remove-item" data-id=${item.id}>remove</span>
+        </div>
+        <div>
+            <i class="fas fa-chevron-up" data-id=${item.id}></i>
+            <p class="item-amount" data-id=${item.id}>${item.amount}</p>
+            <i class="fas fa-chevron-down" data-id=${item.id}></i>
+        </div>
+      `;
+        cartContent.appendChild(div);
+    }
+
+    // Method to open the cart
+    showCart() {
+        cartOverlay.classList.add("transparentBcg");
+        cartDOM.classList.add("showCart");
+    }
+    // Method to close the cart
+    hideCart() {
+        cartOverlay.classList.remove("transparentBcg");
+        cartDOM.classList.remove("showCart");
+    }
+    // Method to inializate things.
+    setupAPP() {
+        cart = Storage.getCart();
+        this.setCartValues(cart);
+        this.populateCart(cart);
+        cartBtn.addEventListener("click", this.showCart);
+        closeCartBtn.addEventListener("click", this.hideCart);
+    }
+    // para cada item del cart del local storage, lo añado
+    populateCart(cart) {
+        cart.forEach((item) => this.addCartItem(item));
+    }
 }
 
-//CARRITO en archivo separado.
+//local storage
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find((product) => product.id === id);
+  }
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+  static getCart() {
+      return localStorage.getItem("cart") 
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+  }
+}
+
+const ui = new UI();
+ui.setupAPP();
+
+ui.displayProducts(products);
+Storage.saveProducts(products);
+
+ui.getBuyButtons();
+ui.cartLogic();
+
+
+//FILTER
+
+// let filterAmd = document.querySelector("#AMD");
+// let filterNvidia = document.querySelector("#NVIDIA");
+
+// filterAmd.addEventListener("click", filter);
+// filterNvidia.addEventListener("click", filter);
+
+// function filter(event) {
+//   // marca retiene la marca del boton clickeado
+//   let marca = event.target.id;
+//   let products = document.querySelectorAll(".product");
+
+//   for (let product of products) {
+//     if (!product.classList.contains(marca)) {
+//       product.classList.toggle("show");
+//     }
+//   }
--- scripts/tarjetas.js
+++ scripts/tarjetas.js
@@ -1,37 +1,37 @@
-let arrayTarjetas= [
+let products = [
     //Serie 120 AMD
-    {id: 1, nombre: "RX 550", marca: "AMD", serie: "500", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/550.webp'},
-    {id: 2, nombre: "RX 560", marca: "AMD", serie: "500", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/560.webp'},
-    {id: 3, nombre: "RX 570", marca: "AMD", serie: "500", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/570.webp'},
-    {id: 4, nombre: "RX 580", marca: "AMD", serie: "500", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/580.webp'},
-    {id: 5, nombre: "RX 590", marca: "AMD", serie: "500", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/590.webp'},
+    {id: 1, name: "RX 550", brand: "AMD", serie: "500", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/550.webp'},
+    {id: 2, name: "RX 560", brand: "AMD", serie: "500", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/560.webp'},
+    {id: 3, name: "RX 570", brand: "AMD", serie: "500", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/570.webp'},
+    {id: 4, name: "RX 580", brand: "AMD", serie: "500", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/580.webp'},
+    {id: 5, name: "RX 590", brand: "AMD", serie: "500", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/590.webp'},
 
     //SERIE 6000 AMD
-    {id: 6, nombre: "RX 6700 XT", marca: "AMD", serie: "6000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6700xt.png'},
-    {id: 7, nombre: "RX 6800 XT", marca: "AMD", serie: "6000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6800xt.png'},
-    {id: 8, nombre: "RX 6900 XT", marca: "AMD", serie: "6000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6900xt.png'},
+    {id: 6, name: "RX 6700 XT", brand: "AMD", serie: "6000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6700xt.png'},
+    {id: 7, name: "RX 6800 XT", brand: "AMD", serie: "6000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6800xt.png'},
+    {id: 8, name: "RX 6900 XT", brand: "AMD", serie: "6000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6900xt.png'},
 
     //SERIE 1000 NVIDIA
-    {id: 9, nombre: "1050", marca: "NVIDIA", serie: "1000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1050.webp'},
-    {id: 10, nombre: "1050ti", marca: "NVIDIA", serie: "1000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1050ti.webp'},
-    {id: 11, nombre: "1060", marca: "NVIDIA", serie: "1000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1060.webp'},
-    {id: 12, nombre: "1070", marca: "NVIDIA", serie: "1000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1070.webp'},
-    {id: 13, nombre: "1070ti", marca: "NVIDIA", serie: "1000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1070ti.webp'},
-    {id: 14, nombre: "1080", marca: "NVIDIA", serie: "1000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1080.webp'},
-    {id: 15, nombre: "1080ti", marca: "NVIDIA", serie: "1000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1080ti.webp'},
+    {id: 9, name: "1050", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1050.webp'},
+    {id: 10, name: "1050ti", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1050ti.webp'},
+    {id: 11, name: "1060", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1060.webp'},
+    {id: 12, name: "1070", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1070.webp'},
+    {id: 13, name: "1070ti", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1070ti.webp'},
+    {id: 14, name: "1080", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1080.webp'},
+    {id: 15, name: "1080ti", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1080ti.webp'},
 
     //SERIE 2000 NVIDIA
-    {id: 16, nombre: "RTX 2060", marca: "NVIDIA", serie: "2000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2060.webp'},
-    {id: 17, nombre: "RTX 2070", marca: "NVIDIA", serie: "2000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2070.webp'},
-    {id: 18, nombre: "RTX 2080", marca: "NVIDIA", serie: "2000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2080.webp'},
-    {id: 19, nombre: "RTX 2080ti", marca: "NVIDIA", serie: "2000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2080ti.webp'},
-    {id: 20, nombre: "RTX 2060s", marca: "NVIDIA", serie: "2000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2060s.webp'},
-    {id: 21, nombre: "RTX 2070s", marca: "NVIDIA", serie: "2000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2070s.webp'},
-    {id: 22, nombre: "RTX 2080s", marca: "NVIDIA", serie: "2000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2080s.webp'},
+    {id: 16, name: "RTX 2060", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2060.webp'},
+    {id: 17, name: "RTX 2070", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2070.webp'},
+    {id: 18, name: "RTX 2080", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2080.webp'},
+    {id: 19, name: "RTX 2080ti", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2080ti.webp'},
+    {id: 20, name: "RTX 2060s", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2060s.webp'},
+    {id: 21, name: "RTX 2070s", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2070s.webp'},
+    {id: 22, name: "RTX 2080s", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2080s.webp'},
 
     //SERIE 3000 NVIDIA -> aún no tengo las imagenes. 
-    // {id: 23, nombre: "RTX 3060", marca: "NVIDIA", serie: "3000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/3060.webp' },
-    // {id: 24, nombre: "RTX 3070", marca: "NVIDIA", serie: "3000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/3070.webp' },
-    // {id: 25, nombre: "RTX 3080", marca: "NVIDIA", serie: "3000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/3080.webp' },
-    // {id: 26, nombre: "RTX 3090", marca: "NVIDIA", serie: "3000", precio: 60000, consumoWatts: 120 , hashrate: 1, gaming: 60, img: 'images/3090.webp' }
+    // {id: 23, name: "RTX 3060", brand: "NVIDIA", serie: "3000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/3060.webp' },
+    // {id: 24, name: "RTX 3070", brand: "NVIDIA", serie: "3000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/3070.webp' },
+    // {id: 25, name: "RTX 3080", brand: "NVIDIA", serie: "3000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/3080.webp' },
+    // {id: 26, name: "RTX 3090", brand: "NVIDIA", serie: "3000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/3090.webp' }
 ]
