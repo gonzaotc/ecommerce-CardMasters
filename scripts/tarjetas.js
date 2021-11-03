c32400ae676b6d367be98b299417e730d6a000a9
gonzaotc <gonza.otc@gmail.com>
2021-11-03 13:53:39 -0300

    added values to sort

--- scripts/tarjetas.js
+++ scripts/tarjetas.js
@@ -1,33 +1,33 @@
 let products = [
     //Serie 120 AMD
-    {id: 1, name: "RX 550", brand: "AMD", serie: "500", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/550.webp'},
-    {id: 2, name: "RX 560", brand: "AMD", serie: "500", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/560.webp'},
-    {id: 3, name: "RX 570", brand: "AMD", serie: "500", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/570.webp'},
-    {id: 4, name: "RX 580", brand: "AMD", serie: "500", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/580.webp'},
-    {id: 5, name: "RX 590", brand: "AMD", serie: "500", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd500/590.webp'},
+    {id: 1, name: "RX 550", brand: "AMD", serie: "500", price: 40000, consumption: 120 , hashrate: 0.37, gaming: 43, img: 'images/amd500/550.webp'},
+    {id: 2, name: "RX 560", brand: "AMD", serie: "500", price: 50000, consumption: 120 , hashrate: 1, gaming: 49, img: 'images/amd500/560.webp'},
+    {id: 3, name: "RX 570", brand: "AMD", serie: "500", price: 60000, consumption: 120 , hashrate: 1.06, gaming: 455, img: 'images/amd500/570.webp'},
+    {id: 4, name: "RX 580", brand: "AMD", serie: "500", price: 110000, consumption: 120 , hashrate: 2.55, gaming: 60, img: 'images/amd500/580.webp'},
+    {id: 5, name: "RX 590", brand: "AMD", serie: "500", price: 140000, consumption: 120 , hashrate: 1, gaming: 70, img: 'images/amd500/590.webp'},
 
     //SERIE 6000 AMD
-    {id: 6, name: "RX 6700 XT", brand: "AMD", serie: "6000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6700xt.png'},
-    {id: 7, name: "RX 6800 XT", brand: "AMD", serie: "6000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6800xt.png'},
-    {id: 8, name: "RX 6900 XT", brand: "AMD", serie: "6000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6900xt.png'},
+    {id: 6, name: "RX 6700 XT", brand: "AMD", serie: "6000", price: 220000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6700xt.png'},
+    {id: 7, name: "RX 6800 XT", brand: "AMD", serie: "6000", price: 250000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6800xt.png'},
+    {id: 8, name: "RX 6900 XT", brand: "AMD", serie: "6000", price: 270000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/amd6000/6900xt.png'},
 
     //SERIE 1000 NVIDIA
-    {id: 9, name: "1050", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1050.webp'},
-    {id: 10, name: "1050ti", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1050ti.webp'},
-    {id: 11, name: "1060", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1060.webp'},
-    {id: 12, name: "1070", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1070.webp'},
-    {id: 13, name: "1070ti", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1070ti.webp'},
-    {id: 14, name: "1080", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1080.webp'},
-    {id: 15, name: "1080ti", brand: "NVIDIA", serie: "1000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia1000/1080ti.webp'},
+    {id: 9, name: "1050", brand: "NVIDIA", serie: "1000", price: 38000, consumption: 120 , hashrate: 0.56, gaming: 60, img: 'images/nvidia1000/1050.webp'},
+    {id: 10, name: "1050ti", brand: "NVIDIA", serie: "1000", price: 55000, consumption: 120 , hashrate: 0.62, gaming: 60, img: 'images/nvidia1000/1050ti.webp'},
+    {id: 11, name: "1060", brand: "NVIDIA", serie: "1000", price: 100000, consumption: 120 , hashrate: 1.93, gaming: 60, img: 'images/nvidia1000/1060.webp'},
+    {id: 12, name: "1070", brand: "NVIDIA", serie: "1000", price: 110000, consumption: 120 , hashrate: 2.43, gaming: 75, img: 'images/nvidia1000/1070.webp'},
+    {id: 13, name: "1070ti", brand: "NVIDIA", serie: "1000", price: 160000, consumption: 120 , hashrate: 2.62, gaming: 75, img: 'images/nvidia1000/1070ti.webp'},
+    {id: 14, name: "1080", brand: "NVIDIA", serie: "1000", price: 145000, consumption: 120 , hashrate: 2.99, gaming: 87, img: 'images/nvidia1000/1080.webp'},
+    {id: 15, name: "1080ti", brand: "NVIDIA", serie: "1000", price: 230000, consumption: 120 , hashrate: 3.70, gaming: 92, img: 'images/nvidia1000/1080ti.webp'},
 
     //SERIE 2000 NVIDIA
-    {id: 16, name: "RTX 2060", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2060.webp'},
-    {id: 17, name: "RTX 2070", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2070.webp'},
-    {id: 18, name: "RTX 2080", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2080.webp'},
-    {id: 19, name: "RTX 2080ti", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2080ti.webp'},
-    {id: 20, name: "RTX 2060s", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2060s.webp'},
-    {id: 21, name: "RTX 2070s", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2070s.webp'},
-    {id: 22, name: "RTX 2080s", brand: "NVIDIA", serie: "2000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/nvidia2000/2080s.webp'},
+    {id: 16, name: "RTX 2060", brand: "NVIDIA", serie: "2000", price: 160000, consumption: 120 , hashrate: 2.62, gaming: 80, img: 'images/nvidia2000/2060.webp'},
+    {id: 17, name: "RTX 2070", brand: "NVIDIA", serie: "2000", price: 170000, consumption: 120 , hashrate: 1, gaming: 83, img: 'images/nvidia2000/2070.webp'},
+    {id: 18, name: "RTX 2080", brand: "NVIDIA", serie: "2000", price: 190000, consumption: 120 , hashrate: 1, gaming: 88, img: 'images/nvidia2000/2080.webp'},
+    {id: 19, name: "RTX 2080ti", brand: "NVIDIA", serie: "2000", price: 240000, consumption: 120 , hashrate: 1, gaming: 92, img: 'images/nvidia2000/2080ti.webp'},
+    {id: 20, name: "RTX 2060s", brand: "NVIDIA", serie: "2000", price: 240000, consumption: 120 , hashrate: 3.35, gaming: 95, img: 'images/nvidia2000/2060s.webp'},
+    {id: 21, name: "RTX 2070s", brand: "NVIDIA", serie: "2000", price: 250000, consumption: 120 , hashrate: 1, gaming: 98, img: 'images/nvidia2000/2070s.webp'},
+    {id: 22, name: "RTX 2080s", brand: "NVIDIA", serie: "2000", price: 260000, consumption: 120 , hashrate: 3.40, gaming: 100, img: 'images/nvidia2000/2080s.webp'},
 
     //SERIE 3000 NVIDIA -> aún no tengo las imagenes. 
     // {id: 23, name: "RTX 3060", brand: "NVIDIA", serie: "3000", price: 60000, consumption: 120 , hashrate: 1, gaming: 60, img: 'images/3060.webp' },
